f32524c045e51a45173a8e627a58a055e743dfac
Ariel Diaz <krafugo@gmail.com>
2019-07-17 14:43:07 -0400

    ADD: create_report function

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < ApplicationController
-  require 'roo'
+  require 'roo-xls'
 
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_report, only: [:show, :edit, :update, :destroy]
@@ -22,8 +22,71 @@ class ReportsController < ApplicationController
     @hotel = Hotel.find_by(params[:hotel_id])
   end
 
-  def create_report
 
+  #Auxiliar function
+  def add_column(sheet, from, to, from_doc)
+    from_doc.column(from).each_with_index do |member, index|
+      sheet[index,to-1] = member #Name of Agency
+    end
+  end
+  
+  def create_report(doc_balance_path, doc_antiquity_path)
+    # Reading documents...
+    doc_antiquity = Roo::Spreadsheet.open(doc_antiquity_path, extension: :xlsx)
+    doc_balance = Roo::Spreadsheet.open(doc_balance_path, extension: :xlsx)
+    
+    #Creating constant...
+    C = 3 #Agencies
+    E = 5
+    F = 6
+    G = 7
+    H = 8
+    I = 9
+    J = 10
+    K = 11
+    L = 12
+    M = 13
+    N = 14
+    O = 15
+    P = 16
+    Q = 17
+    R = 18 
+
+    # Creating report...
+    doc_report = Spreadsheet::Workbook.new
+    sheet1_doc_report = doc_report.create_worksheet
+
+    #Building report...
+    add_column(sheet1_doc_report,C, 1, doc_balance)
+    add_column(sheet1_doc_report,H, E, doc_balance)
+    add_column(sheet1_doc_report,G, F, doc_balance)
+    add_column(sheet1_doc_report,G, M, doc_antiquity)
+    add_column(sheet1_doc_report,I, O, doc_antiquity)
+    add_column(sheet1_doc_report,J, P, doc_antiquity)
+    add_column(sheet1_doc_report,Q, K, doc_antiquity)
+
+    ### From doc_atiquity doing (column E) + (column F)
+    col_E = doc_antiquity.column(E)
+    col_F = doc_antiquity.column(F)
+    col_sum = []
+    len = col_E.length
+    0.upto(len-1) do |i| 
+        begin
+            a = col_E[i].to_f
+            b = col_F[i].to_f
+            col_sum << (a + b).to_s
+        rescue => exception
+            col_sum << "".to_s   
+        end
+    end
+
+    col_sum.each_with_index do |member, index|
+        sheet1_doc_report[index,L-1] = member
+    end
+    #TODO: Make zero to R column
+
+    # #Saving report...
+    doc_report.write './docs/report.xls'
   end
 
   # GET /reports/new
