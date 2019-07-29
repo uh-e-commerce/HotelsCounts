d1163d80c43c0d5142910d2791b881bf8091e8ff
Joel David Hernandez Cruz <jdavidhc1710@gmail.com>
2019-07-29 00:25:19 -0400

    Added User validations for email and name fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: {minimum: 5}
+  validates :email, presence: true
+
   has_many :reports
 end
